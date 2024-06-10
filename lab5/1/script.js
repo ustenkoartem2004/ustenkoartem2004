b539bc5a8516f30d6ab89fa4de451ae4c9a7c94c
ustenkoartem2004 <ustenkoartem2004@gmail.com>
2024-06-10 13:59:51 +0300

    update 5_1lab

--- lab5/1/script.js
+++ lab5/1/script.js
@@ -1,23 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("form").addEventListener('submit', function(event) {
-        event.preventDefault();
+    const form = document.getElementById('form');
+    const status = document.getElementById('status');
 
+    document.getElementById('calculate').addEventListener('click', function(event) {
+        event.preventDefault();
         let age = document.getElementById('age').value;
         let gender = document.querySelector('input[name="gender"]:checked').value;
 
         if (age >= 0 && age <= 17) {
-            alert("Вам работать ещё рано - учитесь")
-            return;
+            status.textContent = "Вам работать ещё рано - учитесь";
         } else if ((age >= 18 && age <= 59 && gender === "male") || (age >= 18 && age <= 54 && gender === "female")) {
-            alert("Вам ещё работать и работать");
+            status.textContent = "Вам ещё работать и работать";
         } else if ((age >= 60 && age <= 64 && gender === "male") || (age >= 55 && age <= 59 && gender === "female")) {
-            alert("Скоро пенсия!");
+            status.textContent = "Скоро пенсия!";
         } else if ((age >= 65 && gender === "male") || (age >= 60 && gender === "female")) {
-            alert("Вам пора на пенсию");
+            status.textContent = "Вам пора на пенсию";
         } else {
-            alert("Да кто ты такой?");
+            status.textContent = "Да кто ты такой?";
         }
+    });
 
-        this.submit();
+    document.getElementById('clear').addEventListener('click', function() {
+        document.getElementById('age').value = '';
+        document.querySelector('input[name="gender"]:checked').checked = false;
+        status.textContent = '';
     });
 });
